04585406a2634c06669e0b1f561b0ee6709269d6
Harsh Sheth <harsh.sheth1128@gmail.com>
2019-08-30 15:06:50 -0400

    Updates

---
+++ precache-manifest.e208dbe7f02cfcc3076314fe228d6b92.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "10a79b3abc908139ccca6b57217f7422",
+    "url": "/index.html"
+  },
+  {
+    "revision": "702f938a6bca1d95c6e4",
+    "url": "/static/css/main.bdc4d58f.chunk.css"
+  },
+  {
+    "revision": "1b07fcb8a1441919e962",
+    "url": "/static/js/2.45bc23ce.chunk.js"
+  },
+  {
+    "revision": "702f938a6bca1d95c6e4",
+    "url": "/static/js/main.dde67035.chunk.js"
+  },
+  {
+    "revision": "ba28c18e88ff0a5b88d9",
+    "url": "/static/js/runtime~main.cad2b5c9.js"
+  },
+  {
+    "revision": "e267b2944ecd9c275f2bd5e5390006ac",
+    "url": "/static/media/Project-Pancake.e267b294.jpg"
+  },
+  {
+    "revision": "6a25b013d8c19ddd002f898928052314",
+    "url": "/static/media/android.6a25b013.jpg"
+  },
+  {
+    "revision": "9123aa53a895b3cd3217cb0dd481b554",
+    "url": "/static/media/email.9123aa53.svg"
+  },
+  {
+    "revision": "a45b6fec9c34de334640a6470d74c637",
+    "url": "/static/media/facebook.a45b6fec.svg"
+  },
+  {
+    "revision": "8a643420d34c1ca43c43a0d483776439",
+    "url": "/static/media/focus.8a643420.png"
+  },
+  {
+    "revision": "e22f7297ea638a812db986ace839e31e",
+    "url": "/static/media/github.e22f7297.svg"
+  },
+  {
+    "revision": "d8631c5e3e343c2d962091d98add4e88",
+    "url": "/static/media/grow.d8631c5e.jpg"
+  },
+  {
+    "revision": "dc3a3ad81ee932ceb4f2565d663113de",
+    "url": "/static/media/instagram.dc3a3ad8.svg"
+  },
+  {
+    "revision": "eb4fde9a01a5b8d568aadc7a170b8ee6",
+    "url": "/static/media/laughing.eb4fde9a.png"
+  },
+  {
+    "revision": "d46654318bd53d49bb3187e53eb571a2",
+    "url": "/static/media/linkedin.d4665431.svg"
+  },
+  {
+    "revision": "89dda22f1f6da6c115fc0f1adad3b056",
+    "url": "/static/media/me.89dda22f.png"
+  },
+  {
+    "revision": "b0ee68c9986f6c8d1c5b74ab67d67008",
+    "url": "/static/media/npm.b0ee68c9.png"
+  },
+  {
+    "revision": "42eeb20938702708c219327476ad737e",
+    "url": "/static/media/phone.42eeb209.svg"
+  },
+  {
+    "revision": "997f3c47e431226b02ce5edbcc29700e",
+    "url": "/static/media/pong.997f3c47.png"
+  },
+  {
+    "revision": "35a2594ddd8061ec95481366aadd68a1",
+    "url": "/static/media/reddit.35a2594d.jpg"
+  },
+  {
+    "revision": "714e837a9d37c3981149ce3ee0bd0a35",
+    "url": "/static/media/tetris.714e837a.jpg"
+  },
+  {
+    "revision": "d238abe819f223d76de9edd818285dc0",
+    "url": "/static/media/waterloo.d238abe8.jpg"
+  },
+  {
+    "revision": "ad238b7bd5a37fbc3622b8dc0042879b",
+    "url": "/static/media/weather.ad238b7b.jpg"
+  }
+]);
--- src/common/ContentSwitch.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const ContentSwitch = ({ index, children }) => {
-  return React.cloneElement(children[index], children[index].props);
-};
-
-export default ContentSwitch;
