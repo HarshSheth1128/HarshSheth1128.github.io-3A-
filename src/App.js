f8c92f5fc12ba7c8b0a43a1fdd6579c41ac6de6f
Harsh Sheth <harsh.sheth1128@gmail.com>
2019-08-31 20:29:36 -0400

    commit to save progress

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.scss';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { HashRouter as Router, Route } from 'react-router-dom';
 import MainPage from './MainPage/MainPage';
 import Line from './common/Line';
 import NavBar from './common/NavBar/NavBar';
@@ -14,7 +14,7 @@ function App() {
         <NavBar />
         <Line margins="50px" />
         <Route path="/" exact component={MainPage} />
-        <Route path="/about" exact component={AboutPage} />
+        <Route path="/about" component={AboutPage} />
         <Route path="/projects" exact component={ProjectsPage} />
         <Line margins="50px" style={{ marginBottom: '100px' }} />
       </Router>
